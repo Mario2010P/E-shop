6b33b8a7cf1db5adffbc79a653acffdf504c46a0
Mariusz Dwornik <mariusz.dwornik@o2.pl>
2020-07-21 20:03:29 +0200

    create CSFR Token

--- shop/static/js/cart.js
+++ shop/static/js/cart.js
@@ -24,6 +24,7 @@ function updateUserOrder(productId, action){
         method:'POST',
         headers:{
             'Content-Type':'application,json'
+            'X-CSRFToken': crsftoken,
         }
         body:JSONstringify({'productId', productId, 'action':action})
     })
