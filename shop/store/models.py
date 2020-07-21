9a1cbe74206e41e68b8be3f894477d2bdef3187e
Mariusz Dwornik <mariusz.dwornik@o2.pl>
2020-07-21 13:17:50 +0200

    add comment

--- shop/store/models.py
+++ shop/store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+#models
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
